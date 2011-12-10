e5306718e0228ca2f02cec727460403206dff25d
Dan P <bikedude880@gmail.com>
2011-12-10 14:55:15 -0800

    Remove overlays temporarily

--- droid2we/droid2we-vendor.mk
+++ droid2we/droid2we-vendor.mk
@@ -27,6 +27,6 @@ PRODUCT_COPY_FILES := \
     packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:/system/etc/permissions/android.software.live_wallpaper.xml
 
 # Pick up overlay for features that depend on non-open-source files
-DEVICE_PACKAGE_OVERLAYS := vendor/motorola/droid2we/overlay
+#DEVICE_PACKAGE_OVERLAYS := vendor/motorola/droid2we/overlay
 
 $(call inherit-product, vendor/motorola/droid2we/droid2we-vendor-blobs.mk)
