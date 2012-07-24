cae7b9ddae015a75dd51270e647ff452ce1c4cc7
bikedude880 <bikedude880@gmail.com>
2012-07-24 14:53:37 -0700

    Use our proprietary JPEG encoder, it actually saves pictures.

--- droid2we/droid2we-vendor-blobs.mk
+++ droid2we/droid2we-vendor-blobs.mk
@@ -119,6 +119,7 @@ PRODUCT_COPY_FILES += \
     vendor/motorola/droid2we/proprietary/lib/libmtp_jni.so:/system/lib/libmtp_jni.so \
     vendor/motorola/droid2we/proprietary/lib/libOMX.TI.720P.Decoder.so:/system/lib/libOMX.TI.720P.Decoder.so \
     vendor/motorola/droid2we/proprietary/lib/libOMX.TI.720P.Encoder.so:/system/lib/libOMX.TI.720P.Encoder.so \
+    vendor/motorola/droid2we/proprietary/lib/libOMX.TI.JPEG.Encoder.so:/system/lib/libOMX.TI.JPEG.Encoder.so \
     vendor/motorola/droid2we/proprietary/lib/libmtpstack.so:/system/lib/libmtpstack.so \
     vendor/motorola/droid2we/proprietary/lib/libnativedrm1.so:/system/lib/libnativedrm1.so \
     vendor/motorola/droid2we/proprietary/lib/liboemcamera.so:/system/lib/liboemcamera.so \
